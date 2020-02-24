4569e5c26d9792455edc1d7694ea78a38f624007
zhaohui <332203372@qq.com>
2020-02-24 12:13:28 +0800

    修改BaseApi为远程

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  // 本地后台
+  // BASE_API: '"http://localhost:8080"'
+  // 远程后台
+  BASE_API: '"http://120.27.63.9:8080"'
 })
